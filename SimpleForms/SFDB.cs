1f6f8cf3057378de8135a5acb29e0fe6eadd575d
T0060assadasdasdasd927D <T0060927@W304A-PC-05>
2018-12-03 09:40:18 +0000

    Adding simple text-based DB system.

---
+++ SimpleForms/SFDB.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleForms
+{
+    class SFDB
+    {
+        //Creating new public properties.
+        public List<string> tables = new List<string>();
+        public string fileLoc;
+
+        //Constructor for database.
+        public SFDB(string location)
+        {
+            //Chceking if database already exists at this location.
+            if (!File.Exists(location+"\\sfdb.txt"))
+            {
+                //No database exists here, create new blank files.
+                File.Create(location + "\\sfdb.txt");
+                Directory.CreateDirectory(location + "\\sfdb");
+                fileLoc = location;
+
+                //Load blanks into strings.
+                tables = new List<string>();
+            } else {
+                //Database exists, load table file.
+                StreamReader sr = new StreamReader(location + "\\sfdb.txt");
+                while (!sr.EndOfStream)
+                {
+                    tables.Add(sr.ReadLine());
+                }
+                sr.Close();
+            }
+        }
+
+        //Methods to create a new table and add a dictionary.
+        public void CreateTable(string tableName)
+        {
+            //Adding to tables.
+            tables.Add(tableName);
+
+            //Creating file in sfdb directory.
+            File.Create(fileLoc + "\\sfdb\\" + tableName+".txt");
+        }
+
+        public void UpdateTable(string table, Dictionary<string, string> d)
+        {
+            //Saving keys and values.
+            StreamWriter sw = new StreamWriter(fileLoc + "\\sfdb\\" + table + ".txt");
+            foreach (var i in d)
+            {
+                //Writing key and keyvalue pair separated by |.
+                sw.WriteLine(i.Key + "|" + i.Value);
+            }
+            sw.Close();
+        }
+
+        //Method to get table.
+        public Dictionary<string,string> GetTable(string table)
+        {
+            //Loading file.
+            StreamReader sr = new StreamReader(fileLoc+"\\sfdb\\"+table+".txt");
+            List<string> keyPair = new List<string>();
+            while (!sr.EndOfStream)
+            {
+                keyPair.Add(sr.ReadLine());
+            }
+            sr.Close();
+
+            //Splitting values out of keypairs.
+            Dictionary<string, string> returnDict = new Dictionary<string, string>();
+            foreach(var i in keyPair)
+            {
+                string[] split = i.Split('|');
+                returnDict.Add(split[0], split[1]);
+            }
+
+            //Returning.
+            return returnDict;
+        }
+    }
+}
